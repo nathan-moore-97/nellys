9894ba821d56298267f11aa85ed06f32c44b196b
Nathan Moore <nmoore@easydynamics.com>
2025-05-13 13:09:15 -0400

    Remove global key load

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` (dev/prod)
-  const env = loadEnv(mode, process.cwd());
+  loadEnv(mode, process.cwd());
   return {
-    // Your config
-    define: {
-      __APP_ENV__: JSON.stringify(env.VITE_GMAPS_API_KEY),
-    },
     plugins: [react()],
   };
 });
