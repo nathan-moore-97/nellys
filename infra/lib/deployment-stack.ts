e7066128ca3b3bb987d25bc938dd53176774f5f0
Nathan Moore <nathanmoore994@gmail.com>
2025-06-20 16:44:33 -0400

    still struggling with nginx

--- infra/lib/deployment-stack.ts
+++ infra/lib/deployment-stack.ts
@@ -78,9 +78,8 @@ export class DeploymentStack extends cdk.Stack {
             'sudo mkdir -p /var/www/nellys-app',
             'sudo cp -r dist/* /var/www/nellys-app/',
             'cd ../infra',
-            // 'sudo mkdir /etc/nginx/default.d/',
-            // 'sudo cp nginx/default /etc/nginx/default.d/default',
-            // 'sudo systemctl start nginx
+            // 'sudo cp nginx/nginx.conf /etc/nginx/',
+            'sudo systemctl start nginx'
         );
 
         apiInstance.addUserData(
