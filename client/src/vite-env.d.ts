0b030bdfc7179b5dbd82e2d645ab52ef3a068159
Nathan Moore <nathanmoore994@gmail.com>
2025-06-23 19:29:07 -0400

    Finally have the react app deploying as intended

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   VITE_GMAPS_API_KEY: string;
+  VITE_API_IP: string;
 }
--- infra/lib/deployment-stack.ts
+++ infra/lib/deployment-stack.ts
@@ -2,8 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
 export class DeploymentStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -35,13 +33,13 @@ export class DeploymentStack extends cdk.Stack {
             allowAllOutbound: true,
         });
 
-        apiSG.connections.allowFrom(reactSG, ec2.Port.tcp(3000));
-
         reactSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.HTTP);
         reactSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.HTTPS);
+        reactSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
 
-        reactSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), "Allow SSH access");
-        apiSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), "Allow SSH access");
+        apiSG.connections.allowFrom(reactSG, ec2.Port.tcp(3000));
+        apiSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
+
 
         // VM for Frontend
         const reactInstance = new ec2.Instance(this, "ReactInstance", {
@@ -68,10 +66,13 @@ export class DeploymentStack extends cdk.Stack {
             }]
         });
 
+        // https://stackoverflow.com/questions/54415841/nodejs-not-installed-successfully-in-aws-ec2-inside-user-data
+
         reactInstance.addUserData(
             'sudo yum install -y git nginx',
             'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash',
-            'source ~/.bashrc',
+            `export NVM_DIR="$HOME/.nvm"`,
+            `[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"`,
             'nvm install --lts',
             'git clone https://github.com/nathan-moore-97/nellys',
             'cd nellys/client/ && npm install && npm run build',
@@ -79,24 +80,20 @@ export class DeploymentStack extends cdk.Stack {
             'sudo cp -r dist/* /var/www/nellys-app/',
             'cd ../infra',
             'sudo cp nginx/nginx.conf /etc/nginx/',
+
+            
             'sudo systemctl start nginx'
         );
 
         apiInstance.addUserData(
-            // 'sudo yum install -y git',
-            // 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash',
-            // 'source ~/.bashrc',
-            // 'nvm install --lts',
-            // 'git clone https://github.com/nathan-moore-97/nellys',
-            // 'cd your-repo/backend && npm install',
-            // 'sudo mkdir -p /data/sqlite',
-            // 'sudo mkfs -t ext4 /dev/nvme1n1', // Modern NVMe naming
-            // 'sudo mount /dev/nvme1n1 /data/sqlite',
-            // 'echo "/dev/nvme1n1 /data/sqlite ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab',
-            // 'echo "DB_PATH=/data/sqlite/db.sqlite" >> .env',
-            // 'npm install -g pm2',
-            // 'pm2 start npm --name "api" -- start',
-            // 'pm2 save'
+            'sudo yum install -y git',
+            'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash',
+            `export NVM_DIR="$HOME/.nvm"`,
+            `[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"`,
+            'nvm install --lts',
+            'git clone https://github.com/nathan-moore-97/nellys',
+            'cd nellys/api && npm install',
+            'npm run start',
         );
 
         new cdk.CfnOutput(this, "ReactAppUrl", {
