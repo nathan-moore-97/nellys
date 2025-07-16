e9df07039dd48c45d6350d295841a32c954e93da
Nathan Moore <nathanmoore994@gmail.com>
2025-07-16 13:50:21 -0400

    collapsing navbar when link is selected

--- client/src/components/NavbarView.tsx
+++ client/src/components/NavbarView.tsx
@@ -1,28 +1,31 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
 function NavbarView() {
+    const [expanded, setExpanded] = useState(false);
+
     return (
-        <Navbar bg="primary" variant="dark" expand="sm" className="mb-4">
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            Nellys Needlers
-          </Navbar.Brand>
-          
-          <Navbar.Toggle aria-controls="basic-navbar" />
-          
-          <Navbar.Collapse id="basic-navbar">
-            <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/calendar">Calendar</Nav.Link>
-              <Nav.Link as={Link} to="/donate">Donate</Nav.Link>
-              <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
-              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <Navbar expanded={expanded} bg="primary" variant="dark" expand="sm" className="mb-4">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    Nellys Needlers
+                </Navbar.Brand>
+                        
+                <Navbar.Toggle onClick={() => {setExpanded(!expanded)}} aria-controls="basic-navbar" />
+
+                <Navbar.Collapse id="basic-navbar" >
+                    <Nav className="ms-auto">
+                        <Nav.Link onClick={() => {setExpanded(false)}} as={Link} to="/calendar">Calendar</Nav.Link>
+                        <Nav.Link onClick={() => {setExpanded(false)}} as={Link} to="/donate">Donate</Nav.Link>
+                        <Nav.Link onClick={() => {setExpanded(false)}} as={Link} to="/gallery">Gallery</Nav.Link>
+                        <Nav.Link onClick={() => {setExpanded(false)}} as={Link} to="/contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
